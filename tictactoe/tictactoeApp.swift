37c82cc7ea4a184cd1bdd378c41094cab52f6c4d
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-09-09 13:50:48 -0600

    Add boilerplate code

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Rajeev R Menon on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
