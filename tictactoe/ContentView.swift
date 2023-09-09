b00765a7c8614014c08aa3d160e3ad47e085c6c3
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-09-09 16:38:11 -0600

    Reformat code and add comments

--- tictactoe/ContentView.swift
+++ tictactoe/ContentView.swift
@@ -7,14 +7,146 @@
 
 import SwiftUI
 
+// Enum to hold various states of the grid
+enum GridState {
+    case empty, circle, cross
+}
+
+// Single square in a grid
+struct GridSquare: View {
+    let size: CGFloat
+    let xIndex: Int
+    let yIndex: Int
+    
+    @Binding var currentState: GridState
+    
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .fill(.black)
+                .frame(width: size, height: size)
+            if currentState == .circle {
+                Image(systemName: "circle")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.white)
+                    .frame(width: size * 0.5)
+            } else if currentState == .cross {
+                Image(systemName: "multiply")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.white)
+                    .frame(width: size * 0.5)
+            }
+            
+        }
+    }
+}
+
+// Main view
 struct ContentView: View {
+    // Array holding the state of each square in the grid
     @State private var grid: [[GridState]] = Array(repeating: Array(repeating: .empty, count: 3), count: 3)
     
+    // Current turn
     @State private var turn: GridState = .cross
     
+    // Current player's symbol
     @State private var currentPlayer: String = "X"
     
+    // Game over or not? Used for triggering the alert
     @State private var gameOver: Bool = false
+    
+    // Function to clear the grid
+    func resetGrid() {
+        for i in 0..<grid.count {
+            for j in 0..<grid[i].count {
+                grid[i][j] = .empty
+            }
+        }
+        currentPlayer = "X"
+        turn = .cross
+    }
+    
+    // Function to open youtube link
+    func openYouTube() {
+        if let youtubeURL = URL(string: "https://www.youtube.com/watch?v=dQw4w9WgXcQ") {
+            UIApplication.shared.open(youtubeURL)
+        }
+    }
+    
+    // Alert when the game is over
+    var gameOverAlert: Alert {
+        var msg: String
+        if turn == .cross{
+            msg = "Player 1 wins!"
+        } else {
+            msg = "Player 2 wins!"
+        }
+        return Alert(
+            title: Text("Game Over"),
+            message: Text(msg),
+            dismissButton: .default(Text("Get the prize!")) {
+                resetGrid()
+                openYouTube()
+            }
+        )
+    }
+    
+    // Function to check if the game is over, returns the winner or nil
+    func checkGameOver() -> GridState? {
+        // Check rows
+        for row in grid {
+            if row.allSatisfy({ $0 == .circle }) {
+                return .circle
+            } else if row.allSatisfy({ $0 == .cross }) {
+                return .cross
+            }
+        }
+        
+        // Check columns
+        for col in 0..<3 {
+            let column = grid.map { $0[col] }
+            if column.allSatisfy({ $0 == .circle }) {
+                return .circle
+            } else if column.allSatisfy({ $0 == .cross }) {
+                return .cross
+            }
+        }
+        
+        // Check diagonals
+        let leftToRightDiagonal = [grid[0][0], grid[1][1], grid[2][2]]
+        let rightToLeftDiagonal = [grid[0][2], grid[1][1], grid[2][0]]
+        
+        if leftToRightDiagonal.allSatisfy({ $0 == .circle }) || rightToLeftDiagonal.allSatisfy({ $0 == .circle }) {
+            return .circle
+        } else if leftToRightDiagonal.allSatisfy({ $0 == .cross }) || rightToLeftDiagonal.allSatisfy({ $0 == .cross }) {
+            return .cross
+        }
+        
+        // The game is still ongoing
+        return nil
+    }
+    
+    // Function to update a particular grid with current turn value
+    func updateGridState(xIndex: Int, yIndex: Int) {
+        if grid[xIndex][yIndex] == .empty {
+            grid[xIndex][yIndex] = turn
+            if turn == .cross {
+                turn = .circle
+                currentPlayer = "O"
+            } else {
+                turn = .cross
+                currentPlayer = "X"
+            }
+        }
+        
+        let winner = checkGameOver() ?? .empty
+        if winner != .empty {
+            turn = winner
+            gameOver = true
+        }
+    }
     
     var body: some View {
         VStack{
@@ -72,128 +204,6 @@ struct ContentView: View {
         }.padding().background(.white)
     }
     
-    var gameOverAlert: Alert {
-        var msg: String
-        if turn == .cross{
-            msg = "Player 1 wins!"
-        } else {
-            msg = "Player 2 wins!"
-        }
-        return Alert(
-            title: Text("Game Over"),
-            message: Text(msg),
-            dismissButton: .default(Text("Get the prize!")) {
-                resetGrid()
-                openYouTube()
-            }
-        )
-    }
-    
-    func checkGameOver() -> GridState? {
-        // Check rows, columns, and diagonals for a winner
-        
-        // Check rows
-        for row in grid {
-            if row.allSatisfy({ $0 == .circle }) {
-                return .circle
-            } else if row.allSatisfy({ $0 == .cross }) {
-                return .cross
-            }
-        }
-        
-        // Check columns
-        for col in 0..<3 {
-            let column = grid.map { $0[col] }
-            if column.allSatisfy({ $0 == .circle }) {
-                return .circle
-            } else if column.allSatisfy({ $0 == .cross }) {
-                return .cross
-            }
-        }
-        
-        // Check diagonals
-        let leftToRightDiagonal = [grid[0][0], grid[1][1], grid[2][2]]
-        let rightToLeftDiagonal = [grid[0][2], grid[1][1], grid[2][0]]
-        
-        if leftToRightDiagonal.allSatisfy({ $0 == .circle }) || rightToLeftDiagonal.allSatisfy({ $0 == .circle }) {
-            return .circle
-        } else if leftToRightDiagonal.allSatisfy({ $0 == .cross }) || rightToLeftDiagonal.allSatisfy({ $0 == .cross }) {
-            return .cross
-        }
-        
-        // The game is still ongoing
-        return nil
-    }
-    
-    func updateGridState(xIndex: Int, yIndex: Int) {
-        if grid[xIndex][yIndex] == .empty {
-            grid[xIndex][yIndex] = turn
-            if turn == .cross {
-                turn = .circle
-                currentPlayer = "O"
-            } else {
-                turn = .cross
-                currentPlayer = "X"
-            }
-        }
-        
-        let winner = checkGameOver() ?? .empty
-        if winner != .empty {
-            turn = winner
-            gameOver = true
-        }
-    }
-    
-    func resetGrid() {
-        for i in 0..<grid.count {
-            for j in 0..<grid[i].count {
-                grid[i][j] = .empty
-            }
-        }
-        currentPlayer = "X"
-        turn = .cross
-    }
-    
-    func openYouTube() {
-        if let youtubeURL = URL(string: "https://www.youtube.com/watch?v=dQw4w9WgXcQ") {
-            UIApplication.shared.open(youtubeURL)
-        }
-    }
-    
-}
-
-enum GridState {
-    case empty, circle, cross
-}
-
-struct GridSquare: View {
-    let size: CGFloat
-    let xIndex: Int
-    let yIndex: Int
-    
-    @Binding var currentState: GridState
-    
-    var body: some View {
-        ZStack{
-            Rectangle()
-                .fill(.black)
-                .frame(width: size, height: size)
-            if currentState == .circle {
-                Image(systemName: "circle")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.white)
-                    .frame(width: size * 0.5)
-            } else if currentState == .cross {
-                Image(systemName: "multiply")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.white)
-                    .frame(width: size * 0.5)
-            }
-            
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
