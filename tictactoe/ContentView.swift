781338d4381a44514292e015916737cbaee78bb8
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-09-09 13:53:02 -0600

    Draw the grid

--- tictactoe/ContentView.swift
+++ tictactoe/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack{
+            GeometryReader{ geometry in
+                VStack {
+                    ForEach(0..<3) { i in
+                        HStack {
+                            ForEach(0..<3) { j in
+                                GridSquare(size: geometry.size.width * 0.3, xIndex: i, yIndex: j)
+                            }
+                        }
+                        .frame(width: geometry.size.width)
+                    }
+                }
+                .frame(height: geometry.size.height)
+            }
+        }.padding()
+        
+    }
+}
+
+struct GridSquare: View {
+    let size: CGFloat
+    let xIndex: Int
+    let yIndex: Int
+    
+    var body: some View {
+        Rectangle()
+            .fill(.gray)
+            .frame(width: size, height: size)
     }
 }
 
